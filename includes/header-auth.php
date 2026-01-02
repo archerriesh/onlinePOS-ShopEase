5c2100bf4107e2d092852e1e179610ebdc99fcd8
archerriesh <2472014@maranatha.ac.id>
2026-01-02 21:08:33 +0700

    css massuk

--- includes/header-auth.php
+++ includes/header-auth.php
@@ -7,12 +7,12 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
 
 <nav class="navbar navbar-expand-lg px-5 pt-4">
-    <a class="navbar-brand fw-bold" href="index.php">ShopEase</a>
+    <a class="navbar-brand fw-bold" href="../index.php">ShopEase</a>
 
     <button class="navbar-toggler" type="button" 
         data-bs-toggle="collapse" 
@@ -26,11 +26,11 @@
     <div class="collapse navbar-collapse" id="authNavbar">
         <div class="ms-auto">
             <?php if ($authPage === 'signin'): ?>
-                <a href="pages/sign-up-page.php" class="btn btn-outline-dark">
+                <a href="../pages/sign-up-page.php" class="btn btn-outline-dark">
                     Sign Up
                 </a>
             <?php else: ?>
-                <a href="pages/sign-in.php" class="btn btn-outline-dark">
+                <a href="../pages/sign-in-page.php" class="btn btn-outline-dark">
                     Sign In
                 </a>
             <?php endif; ?>
