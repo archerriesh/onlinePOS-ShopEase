09ba66624eaeb396fc4e62eede0ff841ebb6c103
Mmayang06 <2472003@maranatha.ac.id>
2026-01-03 17:36:17 +0700

    pengen nangis

---
+++ pages/produk-per-kategori.php
@@ -0,0 +1,76 @@
+<?php
+$pageCSS = '../css/produk-per-kategori.css';
+include '../includes/header-main.php';
+?>
+
+<div class="shopease-wrapper">
+  <aside class="sidebar">
+    <div class="menu-title">☰ Categories</div>
+    <ul class="category-list">
+      <li>electronics</li>
+      <li>Man clothes</li>
+      <li>Woman clothes</li>
+      <li>Man Shoes</li>
+      <li>Woman Shoes</li>
+      <li>Bags</li>
+      <li>Skincare & Makeup</li>
+      <li>Hobbies</li>
+      <li>Mom & Baby</li>
+    </ul>
+  </aside>
+
+  <main class="content">
+    <div class="product-grid">
+      <div class="product-card">
+        <img src="../assets/huawei.png" alt="">
+        <h3>Huawei Matepad 11.5</h3>
+        <p>Rp. 5.589.000</p>
+      </div>
+
+      <div class="product-card">
+        <img src="../assets/iphone.png" alt="">
+        <h3>Iphone 14<br>128GB,Blue</h3>
+        <p>Rp. 9.000.000</p>
+      </div>
+
+      <div class="product-card">
+        <img src="../assets/watch.png" alt="">
+        <h3>Apple Watch SE 3<br>Aluminium case</h3>
+        <p>Rp. 4.499.000</p>
+      </div>
+
+      <div class="product-card">
+        <img src="../assets/asus.png" alt="">
+        <h3>Asus Zenbook 14<br>OLED UX3405CA</h3>
+        <p>Rp. 22.999.000</p>
+      </div>
+
+      <div class="product-card">
+        <img src="../assets/earbuds.png" alt="">
+        <h3>Baseus BP1 NC<br>TWS Active Noise</h3>
+        <p>Rp. 249.000</p>
+      </div>
+
+      <div class="product-card">
+        <img src="../assets/robot.png" alt="">
+        <h3>Dreame D9 Max<br>Gen 2 Robot</h3>
+        <p>Rp. 3.790.000</p>
+      </div>
+
+      <div class="product-card">
+        <img src="../assets/airfryer.png" alt="">
+        <h3>Philips Airfryer Low<br>Watt</h3>
+        <p>Rp. 783.000</p>
+      </div>
+
+      <div class="product-card">
+        <img src="../assets/juicer.png" alt="">
+        <h3>Ecohome Slow<br>Juicer</h3>
+        <p>Rp. 1.280.000</p>
+      </div>
+    </div>
+  </main>
+</div>
+
+
+<?php include '../includes/footer.php'; ?>
